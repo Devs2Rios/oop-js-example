89e926fcf1d8208d6aa88cf47428cc9acc7251e9
eduairet <hola@eduairet.com>
2023-09-07 22:04:12 -0600

    Map and form added

--- script.js
+++ script.js
@@ -10,3 +10,55 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+let map, mapEvent;
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude, longitude } = position.coords;
+    map = L.map('map').setView([latitude, longitude], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    map.on('click', function (e) {
+      mapEvent = e;
+      form.classList.remove('hidden');
+      inputDistance.focus();
+    });
+  },
+  function (_) {
+    alert('Could not get your position');
+  }
+);
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  // Clean input fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+  // Get coords from map click event
+  const { lat, lng } = mapEvent.latlng;
+  // Add marker on click event
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Running!')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function (e) {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
